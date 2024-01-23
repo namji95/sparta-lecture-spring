b7b57f37e37ef2c6a1b6890d58a1468754d9cd34
namji <skawlgns07@naver.com>
2024-01-23 11:16:21 +0900

    Spring 다양한 방법으로 api 요청 실습 다른 방법
    Fix: 다양한 방법으로 api 요청을 하기 위한 request 패키지의 RequestController 클래스를 templates 파일에 request-form.html을 이용하여 추가하였습니다.
    PostMapping 요청 방식으로 ModelAttribute 어노테이션을 파라미터에 담아 api 통신하는 방법,
    GetMapping 요청 방식으로 ModelAttribute 어노테이션을 파라미터에 담아 api 통신하는 방법,
    PostMapping 요청 방식으로 RequestBody 어노테이션을 파라미터에 담아 api 통신하는 방법

--- src/main/java/com/sparta/springmvc/request/RequestController.java
+++ src/main/java/com/sparta/springmvc/request/RequestController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/hello/request")
 public class RequestController {
 
+    /*
+    스프링은 보통 파라미터가 SimnpleValue 타입이면 RequestParam으로 판단하고
+     (SimpleValue : int같은(원시타입) / Integer(래퍼타입)같은 / Date같은),
+     그렇지 않다면 ModelAttribute라고 판단합니다.
+     */
+
     @GetMapping("/form/html")
     public String helloForm() {
         return "hello-request-form";
@@ -64,5 +70,50 @@ public class RequestController {
     @ResponseBody
     public String helloPostRequestParam(@RequestParam String name, @RequestParam int age) {
         return String.format("Hello, @RequestParam.<br> name = %s, age = %d", name, age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/model
+    // Header
+    //  Content type: application/x-www-form-urlencoded
+    // Body
+    //  name=Robbie&age=95
+    /*
+    Body로 데이터가 name=Robbie&age=95 이렇게 들어왔을 때 객체로 처리할 수 있는데
+    이 방법이 ModelAttribute 어노테이션을 파라미터로 담으면 됩니다.
+     */
+    @PostMapping("/form/model")
+    @ResponseBody
+    public String helloRequestBodyForm(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // GET http://localhost:8080/hello/request/form/param/model?name=Robbie&age=95
+    /*
+    ModelAttribute는 Body에 Qeury String 방식으로 데이터가 반환됐다면
+    Query String에 들어오는 데이터가 점점 많아지면 코드가 복잡해지고 유지보수가 힘들어집니다.
+    밑 코드는 ModelAttribute는 생략해도 동작은 하지만
+     */
+    @GetMapping("/form/param/model")
+    @ResponseBody
+    public String helloRequestParam(@ModelAttribute Star star) {
+        return String.format("Hello, @ModelAttribute.<br> (name = %s, age = %d) ", star.name, star.age);
+    }
+
+    // [Request sample]
+    // POST http://localhost:8080/hello/request/form/json
+    // Header
+    //  Content type: application/json
+    // Body
+    //  {"name":"Robbie","age":"95"}
+    /*
+    PostMapping을 사용한 RequestBody 파라미터는 Body에 Json 형식으로 데이터를 반환합니다.
+    밑 코드는 RequestBody 어노테이션을 꼭 달아줘야 합니다.
+     */
+    @PostMapping("/form/json")
+    @ResponseBody
+    public String helloPostRequestJson(@RequestBody Star star) {
+        return String.format("Hello, @RequestBody.<br> (name = %s, age = %d) ", star.name, star.age);
     }
 }
