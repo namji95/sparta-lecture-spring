5c47356eec89f3e07729a8c6862f4d55c4f11a0b
namji <skawlgns07@naver.com>
2024-01-24 11:17:29 +0900

    Spring 중복 코드 제거
    Feat: 중복 코드 제거
    중복 코드 제거를 위한 memo 패키지의 클래스들을 memoIoCDI 패키지로 복사한 후 memo 패키지의 클래스들을 주석 처리하고
    memoIoCDI 패키지의 Controller와 Service 클래스의 중복 코드를 제거했습니다.

--- src/main/java/com/sparta/week2/memo/dto/MemoRequestDto.java
+++ src/main/java/com/sparta/week2/memo/dto/MemoRequestDto.java
@@ -1,9 +1,9 @@
-package com.sparta.week2.memo.dto;
-
-import lombok.Getter;
-
-@Getter
-public class MemoRequestDto {
-    private String username;
-    private String contents;
-}
+//package com.sparta.week2.memo.dto;
+//
+//import lombok.Getter;
+//
+//@Getter
+//public class MemoRequestDto {
+//    private String username;
+//    private String contents;
+//}
---
+++ src/main/java/com/sparta/week2/memoIoCDI/dto/MemoRequestDto.java
@@ -0,0 +1,9 @@
+package com.sparta.week2.memoIoCDI.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
+}
