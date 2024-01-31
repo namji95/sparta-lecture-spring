25fd8b8d299588000805e46bab18d20ba981a1cd
namji <skawlgns07@naver.com>
2024-02-01 00:13:42 +0900

    Spring JPA Auditing 기능 추가
    Feat: JPA Auditing 기능으로 생성 시간과 수정 시간이 저장될 수 있게 구현
    entity 패키지에 auditing 기능을 구현할 클래스를 생성하고 entity 클래스에서 상속 받습니다.
    실행을 담당하는 main 클래스에서 auditing 기능을 사용할 거라는 것을 알려주는 어노테이션도 추가하여
    데이터 저장 시 저장 시간이 추가되는 기능과 수정 시 저장 시간을 고정되고 수정 시간만 저장될 수 있게 구현하였습니다.

--- src/main/java/com/sparta/SpringMvcApplication.java
+++ src/main/java/com/sparta/SpringMvcApplication.java
@@ -2,8 +2,11 @@ package com.sparta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
+// @EnableJpaAuditing : Auditing 기능을 사용하겠다고 알려주는 어노테이션
 public class SpringMvcApplication {
 
     /*
--- src/main/java/com/sparta/week2/memoJpa/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/week2/memoJpa/dto/MemoResponseDto.java
@@ -3,21 +3,21 @@ package com.sparta.week2.memoJpa.dto;
 import com.sparta.week2.memoJpa.entity.Memo;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MemoResponseDto {
     private final Long id;
     private final String username;
     private final String contents;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public MemoResponseDto(Memo memo) {
         this.id = memo.getId();
         this.username = memo.getUsername();
         this.contents = memo.getContents();
-    }
-
-    public MemoResponseDto(Long id, String username, String contents) {
-        this.id = id;
-        this.username = username;
-        this.contents = contents;
+        this.createdAt = memo.getCreatedAt();
+        this.modifiedAt = memo.getModifiedAt();
     }
 }
