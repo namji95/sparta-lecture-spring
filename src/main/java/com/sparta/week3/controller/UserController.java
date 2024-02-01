1bd11117953a9656dffea309d3d58edb7c979b4c
namji <skawlgns07@naver.com>
2024-02-02 02:23:43 +0900

    회원 가입 기능 구현
    Feat: Token을 이용한 회원 가입 구현
    관리자로 회원가입 하기 위해 Token 지정 후
    회원 가입 시 관리자로 회원가입 하겠다는 버튼을 클릭하지 않으면 일반 사용자로 회원가입 되고,
    관리자로 회원가입 하겠다는 버튼을 클릭하면 관리자 Token을 입력하는 입력창이 나오고 거기에 Token을 입력하면 관리자로 회원가입 되게 구현

---
+++ src/main/java/com/sparta/week3/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.sparta.week3.controller;
+
+import com.sparta.week3.dto.SignupRequestDto;
+import com.sparta.week3.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController (UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user/login-page")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/user/signup")
+    public String signupPage() {
+        return "signup";
+    }
+
+    @PostMapping("/user/signup")
+    public String signup(SignupRequestDto requestDto) {
+        userService.signup(requestDto);
+
+        return "redirect:/api/user/login-page";
+    }
+}
---
+++ src/main/java/com/sparta/week3/entity/User.java
@@ -0,0 +1,37 @@
+package com.sparta.week3.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING) // 이넘 타입을 데이터베이스에 저장할 때 사용하는 어노테이션
+    private UserRoleEnum role;
+
+    public User (String username, String password, String email, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+}
