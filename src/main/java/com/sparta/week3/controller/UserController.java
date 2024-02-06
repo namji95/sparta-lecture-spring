54d610691bdee07eb72ac3396aca06350bd76221
namji <skawlgns07@naver.com>
2024-02-06 16:35:37 +0900

    Validation 예외 처리
    Feat: Validation 처리한 데이터에 형식에 맞지 않는 값 입력하여 예외처리한 결과 확인하기

--- src/main/java/com/sparta/week3/controller/UserController.java
+++ src/main/java/com/sparta/week3/controller/UserController.java
@@ -1,22 +1,26 @@
 package com.sparta.week3.controller;
 
-import com.sparta.week3.dto.LoginRequestDto;
 import com.sparta.week3.dto.SignupRequestDto;
 import com.sparta.week3.service.UserService;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
 
-    public UserController (UserService userService) {
+    public UserController(UserService userService) {
         this.userService = userService;
     }
 
@@ -31,11 +35,21 @@ public class UserController {
     }
 
     @PostMapping("/user/signup")
-    public String signup(SignupRequestDto requestDto) {
+    public String signup(@Valid SignupRequestDto requestDto, BindingResult bindingResult) {
+        // Validation 예외처리
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if (fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+            return "redirect:/api/user/signup";
+        }
+
         userService.signup(requestDto);
 
         return "redirect:/api/user/login-page";
     }
+}
 
 //    filter에서 login 기능 처리할 수 있게 하기 위해 주석
 //    @PostMapping("/user/login")
@@ -48,4 +62,3 @@ public class UserController {
 //
 //        return "redirect:/";
 //    }
-}
