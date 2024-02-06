1e557c578ac0f91a59eb9d5303e496d036f5f9ad
namji <skawlgns07@naver.com>
2024-02-06 15:42:12 +0900

    데이터 검증하기
    Feat: 데이터 검증 Validation 처리하기
    입력받는 데이터가 형식에 맞게 잘 입력되고 있는지 검증하고
    형식에 맞지 않으면 다음으로 넘어가지 않게 하기 위한 기능 구현

--- src/main/java/com/sparta/week3/controller/ProductController.java
+++ src/main/java/com/sparta/week3/controller/ProductController.java
@@ -1,14 +1,15 @@
 package com.sparta.week3.controller;
 
+import com.sparta.week3.dto.ProductRequestDto;
 import com.sparta.week3.entity.User;
 import com.sparta.week3.entity.UserRoleEnum;
 import com.sparta.week3.security.UserDetailsImpl;
+import jakarta.validation.Valid;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api")
@@ -33,5 +34,11 @@ public class ProductController {
         }
 
         return "redirect:/";
+    }
+
+    @PostMapping("/validation")
+    @ResponseBody
+    public ProductRequestDto testValid(@RequestBody @Valid ProductRequestDto requestDto) {
+        return requestDto;
     }
 }
